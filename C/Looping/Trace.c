e631f0c74f96ecab390a23d6e0702cdbdabdea36
Prabhu Ram <prabhuramn4186@gmail.com>
2022-05-29 07:28:27 +0530

    Try above code
    Try above code as per instructions given in comments on each line one after another

--- C/Looping/Trace.c
+++ C/Looping/Trace.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 int main()
@@ -9,6 +8,7 @@ int main()
     printf("Enter Matrix Row and column size");    // Provinding message to user as Enter Matrix Row and column size
     scanf("%d%d",&Maxrow, &Maxcol);                 // Collect value for row first and then col next
     // Next few lines of code upto next comment is for loading data into MatA variable
+    printf("Enter Elements of Matrix one by one\n");
     for(i=0;i<Maxrow;i++)           // i based for loop gets incremented only when inner loops completed
     {
         for(j=0;j<Maxcol;j++)       // j based for loop gets increses upon execution of its block of statements
@@ -19,19 +19,69 @@ int main()
     }
     
     // To Display the inputed elements of Matrix , following code will execute
+    printf("\nMatrix A as follows\n");
     for(i=0;i<Maxrow;i++)
     {
         for(j=0;j<Maxcol;j++)
         {
-            printf("%d",MatA[i][j]);
-            //printf("%d\t",MatA[i][j]);         //Note 1: First Comment above line and Uncomment this Line and see How you displayed Matrix concent 
+           printf("%d",MatA[i][j]);
+           // printf("%d\t",MatA[i][j]);         //Note 1: First Comment above line and Uncomment this Line and see How you displayed Matrix concent 
             
         }
-       // printf("\n");                         // After execute Note 1: inference, now get another inference by uncomment this line, and see how matrix displays
+        //printf("\n");                         // After execute Note 1: inference, now get another inference by uncomment this line, and see how matrix displays
        // Can you able to get insights why above printf("\n") statement is put outside inner for loop
     }
-    exit(0);                          // Upto now you got skill of Loading values/ elements of Matrix and Displaying elements of Matrix one by one.
+   // exit(0);                          // Upto now you got skill of Loading values/ elements of Matrix and Displaying elements of Matrix one by one.
     // Are you clear , shall we go further comment above statement exit(0) and proceed further
     //Here code of looping for finding Trace of Matrix
+    /*
+    Suppose Matrix looks like follows
+    1   2   3
+    10  20  30
+    -1  -10 -10
+    Trace of Matrix if sum of diagonal elements of Matrix
+    For elementwise 2D array handling we have to use 2 level of nested looping, 3D means 3 level of nested looping and so on...
+    */
+    // Following code is elementwise handling of 2D array(Matrix)
+    for(i=0;i<Maxrow;i++)
+    {
+        for(j=0;j<Maxcol;j++)
+        {
+            printf("Row No. %d,Column No. %d of Matrix element is %d\n",i,j,MatA[i][j]);// Here it gives elements of matrix as rowwise
+            // Comment above line and uncomment following line for column wise accessing and Displaying
+            //printf("Row No. %d,Column No. %d of Matrix element is %d\n",i,j,MatA[j][i]);;// Here it gives elements of matrix as column wise
+        }
+    }
+    //exit(0);
+    // Are you clear , shall we go further comment above statement exit(0) and proceed further
+    //Now you know how to access elementwise by nexted looping
+    // Lets start coding for finding trace of matrix, in which sum of elements whose indexes of row and column to be same
+    int sum = 0;        // Resultant holder of data type "int32" with initial value as 0
+    for(i=0;i<Maxrow;i++)
+    {
+        for(j=0;j<Maxcol;j++)
+        {
+            if(i==j)
+            {
+                sum = sum + MatA[i][j];
+            }
+            else
+            {
+                printf("Addition is not processed as it is not diagonal elements\n");
+            }
+        
+        }
+    }
+    printf("Trace of Matrix :\t %d\n",sum);
+    printf("\n Another Approach \n\n");
+    // Lets start coding for finding trace of matrix in simple approach as diagonal elements indexes should be same so lets use same value holder for both row & column accessing as follows
+    int sum = 0;        // Resultant holder of data type "int32" with initial value as 0
+    for(i=0;i<Maxrow;i++)
+    {
+        sum = sum + MatA[i][i];   // same i value will be given to point corresponding elements in matrix, as MatA[0][0],MatA[1][1] and so on...
+    }
+    printf("Trace of Matrix :\t %d\n",sum);
+    
     return 0;
 }
+
